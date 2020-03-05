28debb31b663bfff7f1588da990405eab97e46b3
Haider <hai.tiwana@gmail.com>
2020-03-05 12:42:23 -0800

    add reselect dependency, update cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  selectCart,
+  cart => cart.cartItems
+);
+
+// NOTE: Selectors are composable. They can be used as input to other selectors.
+export const selectCartItemsCount = createSelector(selectCartItems, cartItems =>
+  cartItems.reduce(
+    (accumulatedQty, cartItem) => accumulatedQty + cartItem.quantity,
+    0
+  )
+);
